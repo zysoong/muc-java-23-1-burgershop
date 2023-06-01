d01004b4aca2a11bdd19c8df2431294083b6cdd6
Ziyang Song <ziyang.song@gmx.de>
2023-06-01 09:43:06 +0200

    Initial commit

---
+++ src/main/java/de/iav/mucjava231burgershop/MucJava231BurgershopApplication.java
@@ -0,0 +1,13 @@
+package de.iav.mucjava231burgershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MucJava231BurgershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MucJava231BurgershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/iav/mucjava231burgershop/MucJava231BurgershopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iav.mucjava231burgershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MucJava231BurgershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
