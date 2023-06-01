39fffca0bdc5d0a279cdd879d224e2d2e056d10e
Ziyang Song <ziyang.song@gmx.de>
2023-06-01 11:43:13 +0200

    Add menu controller

--- src/main/java/de/iav/mucjava231burgershop/controller/MenuController.java
+++ src/main/java/de/iav/mucjava231burgershop/controller/MenuController.java
@@ -1,9 +1,46 @@
 package de.iav.mucjava231burgershop.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.iav.mucjava231burgershop.model.Menu;
+import de.iav.mucjava231burgershop.service.MenuService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/burgershop")
+@RequestMapping("/api/menus")
 public class MenuController {
+
+    private final MenuService menuService;
+
+    public MenuController(MenuService menuService){
+        this.menuService = menuService;
+    }
+
+    @GetMapping
+    public List<Menu> listMenus(){
+        return this.menuService.listMenus();
+    }
+
+    @GetMapping("/{id}")
+    public Menu getMenuById(@PathVariable String id)
+    {
+        return this.menuService.getMenuById(id);
+    }
+
+    @PostMapping
+    public void addMenu(Menu menu)
+    {
+        this.menuService.addMenu(menu);
+    }
+
+    @PutMapping("/{id}")
+    public void updateMenu(@PathVariable String id, @RequestBody Menu menu){
+        this.menuService.updateMenu(id, menu);
+    }
+
+    @DeleteMapping("/{id}")
+    public void updateMenu(@PathVariable String id){
+        this.menuService.deleteMenu(id);
+    }
+
 }
